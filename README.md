30606c73b907b8a21e43c9a4b9666dc376991c9e
Alonso Massa <joalmr@gmail.com>
2025-03-27 02:32:09 -0500

    sin pnpm

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Página personal, proyectos y experiencia
 
 @astrojs/check
 "check": "biome check --apply-unsafe ."
+"packageManager": "pnpm@9.12.2",
--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -3,25 +3,21 @@ const domain = import.meta.env.STRAPI_DOMAIN;
 const apiUrl = `${domain}/api`;
 
 export const getGlobal = async () => {
-    console.log("getGlobal");
-    
     const res = await fetch(`${apiUrl}/global`);
     if (!res.ok) throw new Error("Failed to fetch global info");
 
     const data = await res.json();
-    console.log(data);
+    console.log("getGlobal",data);
     
     return data;
 };
 
 export const getPages = async () => {
-    console.log("getPages");
-    
     const res = await fetch(`${apiUrl}/pages`);
     if (!res.ok) throw new Error("Failed to fetch pages");
 
     const data = await res.json();
-    console.log(data);
+    console.log("getPages",data);
     
     return data;
 };
